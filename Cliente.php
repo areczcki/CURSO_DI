80858c05bdc249c5d3bbaa16004ed34bf667bf75
alexandre <alexandrebezao@gmail.com>
2017-06-06 21:48:54 -0300

    Aula 4 - Global

--- Cliente.php
+++ Cliente.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'conexao.php';
+require_once 'Conexao.php';
 
 class Cliente
 {
--- conexao.php
+++ conexao.php
@@ -1,7 +1,24 @@
 <?php
-$host = "localhost";
-$db = "diservice";
-$user = "root";
-$password = "root";
 
-$db = new \PDO("msql:host={$host};dbname={$db}",$user,$password);
+class Conexao
+{
+
+	private $host;
+	private $db;
+	private $user;
+	private $password;
+
+	public function __construct($host, $db, $user, $password)
+	{
+		$this->host		= $host;
+		$this->db		= $db;
+		$this->user		= $user;
+		$this->password = $password;
+	}
+
+	public function connect()
+	{
+		return new \PDO("msql:host={$this->host};dbname={$this->db}",$this->user,$this->password);
+	}
+
+}

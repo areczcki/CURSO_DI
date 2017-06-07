1de6de766e41dd02b70ecae1a2f8c4eb03f054d9
alexandre <alexandrebezao@gmail.com>
2017-06-06 21:30:35 -0300

    Aula 2 - Criando Listagem de Clientes

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
 require 'conexao.php';
+
+$query = "select * from clientes";
+$stmt = $db->prepare($query);
+$stmt->execute();
+
+$clientes = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+?>
+
+<table>
+	<tr>
+		<td>ID</td>
+		<td>Nome</td>
+		<td>Email</td>
+	</tr>
+	<? foreach($clientes as $cliente): ?>
+	<tr>
+		<td><?$cliente['id']?></td>
+		<td><?$cliente['nome']?></td>
+		<td><?$cliente['email']?></td>
+	</tr>
+	<? endforeach; ?>
+</table>
